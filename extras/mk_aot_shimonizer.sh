6db35b2a1e4d7a8bc37a8aaeabfa92a778c13522
sasaki <administrator@ultra-unko.com>
2016-02-19 18:14:20 +0900

    AOTｼﾓﾅｲｻﾞー追加

---
+++ extras/mk_aot_shimonizer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ "x" = "x${OUT}" ];then
+	OUT=shimonizer.sh
+fi
+SRC_URL=https://raw.githubusercontent.com/sazae657/Shimonizer/master/Shimonizer.cs
+SRC=../Shimonizer.cs
+cat template/aot_heaer.txt >${OUT}
+if [ "x-online" = "x$@" ];then
+	curl ${SRC_URL} >.tmp.cs 
+else
+	cat ${SRC}  >.tmp.cs
+fi
+
+mcs /t:exe  /out:.Shimonizer.exe /r:System.Xml.Linq .tmp.cs || exit
+
+cat .Shimonizer.exe | gzip -9 | uuencode -m Shimonizer.exe >>${OUT}
+
+cat template/aot_footer.txt >>${OUT}
+
+chmod +x ${OUT}
+#rm -f .Shimonizer.exe .tmp.cs
+
